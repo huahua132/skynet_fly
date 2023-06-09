efdba8953a3c23a6357dc6804e55dbbc636ea485
Faker2 <faker@git.com>
2023-06-09 17:00:45 +0800

    code_cache test

---
+++ examples/code_cache_test/main.lua
@@ -0,0 +1,19 @@
+local skynet = require "skynet"
+skynet.start(function()
+	-- local cur_time = skynet.time()
+	-- for i = 1,10000 do
+	-- 	skynet.newservice('hot_container_2','test_m')
+	-- end
+	-- skynet.error("use time:",skynet.time() - cur_time)
+	-- not cache 3.69 s
+
+	local cur_time = skynet.time()
+	for i = 1,10000 do
+		skynet.newservice('test')
+	end
+	skynet.error("use time:",skynet.time() - cur_time)
+
+	--cache 2.65 s
+
+	--关闭code cache 启动服务性能下降28%
+end)
