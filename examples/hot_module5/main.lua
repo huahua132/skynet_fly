440e8c1181d5846023f1a1193ec95cc77d63d1e9
Faker2 <faker@git.com>
2023-06-20 18:04:55 +0800

    hot_module5

--- examples/hot_module5/main.lua
+++ examples/hot_module5/main.lua
@@ -5,17 +5,15 @@ skynet.start(function()
 	skynet.error("start hot_module5!!!>>>>>>>>>>>>>>>>>")
 	local cmgr = skynet.uniqueservice('contriner_mgr')
 
-	skynet.call(cmgr,'lua','load_module',"server_m",1)
+	skynet.call(cmgr,'lua','load_module',"service_m",1,{{player_num = 2}})
 	skynet.call(cmgr,'lua','load_module',"agent_m",1)
 
-	contriner_client:new("agent_m",function()
-		return false
-	end)
+	local client = contriner_client:new("agent_m",function() end)
 	for i = 10001,10002 do
 		local player = {
 			player_id = i,
 			nickname = "player_" .. i
 		}
-		contriner_client:mod_call("enter",player)
+		client:mod_call("enter",player)
 	end
 end)
