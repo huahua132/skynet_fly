698245353c3a0d1383870bf4d35e4b9ffbd2cc53
Faker2 <faker@git.com>
2023-06-20 11:20:19 +0800

    热更方案3实现

--- service/contriner_mgr_3.lua
+++ service/contriner_mgr_3.lua
@@ -10,8 +10,11 @@ local skynet_call = skynet.call
 local skynet_pack = skynet.pack
 local skynet_ret = skynet.ret
 
+local NORET = {}
+
 local g_module_id_list_map = {}
 local g_module_watch_map = {}
+local g_module_version_map = {}
 
 local CMD = {}
 
@@ -30,37 +33,72 @@ function CMD.load_module(source,module_name,launch_num)
 	for _,id in ipairs(old_id_list) do
 		skynet_send(id,'lua','exit')
 	end
+
 	g_module_id_list_map[module_name] = id_list
 
-	local watch_map = g_module_watch_map[module_name]
-	if watch_map then
-		for src in pairs(watch_map) do
-			local session = skynet_send(src,'lua','watchrsp',module_name,id_list)
-			if not session then
-				skynet.error("send invaild address ",src)
-			end
-		end
+	if not g_module_version_map[module_name] then
+		g_module_version_map[module_name] = 0
 	end
-	
-	return id_list
-end
 
-function CMD.watch(source,module_name)
-    assert(module_name,'not module_name')
-    if not g_module_watch_map[module_name] then
+	if not g_module_watch_map[module_name] then
 		g_module_watch_map[module_name] = {}
 	end
+	
+	g_module_version_map[module_name] = g_module_version_map[module_name] + 1
+	local version = g_module_version_map[module_name]
 
 	local watch_map = g_module_watch_map[module_name]
-	watch_map[source] = true
-	return g_module_id_list_map[module_name]
+	for source,response in pairs(watch_map) do
+		response(true,id_list,version)
+		watch_map[source] = nil
+	end
+	
+	return id_list,version
+end
+
+function CMD.query(source,module_name)
+	assert(module_name,'not module_name')
+	assert(g_module_id_list_map[module_name])
+	assert(g_module_version_map[module_name])
+
+	local id_list = g_module_id_list_map[module_name]
+	local version = g_module_version_map[module_name]
+
+	return id_list,version
+end
+
+function CMD.watch(source,module_name,version)
+    assert(module_name,'not module_name')
+	assert(version,"not version")
+	assert(g_module_id_list_map[module_name])
+	assert(g_module_version_map[module_name])
+
+	local id_list = g_module_id_list_map[module_name]
+	local version = g_module_version_map[module_name]
+	local watch_map = g_module_watch_map[module_name]
+
+	assert(not watch_map[source])
+	if version ~= version then
+		return id_list,version
+	end
+
+	watch_map[source] = skynet.response()
+	return NORET
 end
 
 function CMD.unwatch(source,module_name)
 	assert(module_name,'not module_name')
-	assert(g_module_watch_map[module_name])
+	assert(g_module_id_list_map[module_name])
+	assert(g_module_version_map[module_name])
 
+	local id_list = g_module_id_list_map[module_name]
+	local version = g_module_version_map[module_name]
 	local watch_map = g_module_watch_map[module_name]
+	local response = watch_map[source]
+	assert(response)
+
+	skynet.error("unwatch:",module_name,version)
+	response(true,id_list,version)
 	watch_map[source] = nil
 	return true
 end
@@ -68,8 +106,12 @@ end
 skynet.start(function()
 	skynet.register('.contriner_mgr_3')
 	skynet.dispatch('lua',function(session,source,cmd,...)
+		skynet.error("dispatch:",source,cmd,...)
 		local f = CMD[cmd]
 		assert(f,'cmd no found :'..cmd)
-		skynet_ret(skynet_pack(f(source,...)))
+		local r1,r2 = f(source,...)
+		if r1 ~= NORET then
+			skynet_ret(skynet_pack(r1,r2))
+		end
 	end)
 end)
