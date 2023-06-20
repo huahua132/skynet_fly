136ec79784276a2333e66ae5f54b63856092c65f
Faker2 <faker@git.com>
2023-06-20 17:58:12 +0800

    hot_module5

---
+++ examples/hot_module5/module/agent_m.lua
@@ -0,0 +1,21 @@
+local contriner_client = require "contriner_client"
+
+local M = {}
+
+function M.enter(player)
+
+end
+
+function M.start()
+
+end
+
+function M.exit()
+
+end
+
+function M.init()
+
+end
+
+return M
