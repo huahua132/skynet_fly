541ebccc9e25a4638cfde85654de7d669c6b823c
Faker2 <faker@git.com>
2023-06-20 12:24:52 +0800

    hot_module

---
+++ service/contriner_mgr.lua
@@ -0,0 +1,115 @@
+local skynet = require "skynet.manager"
+
+local assert = assert
+local ipairs = ipairs
+local pairs = pairs
+local tinsert = table.insert
+local tremove = table.remove
+local skynet_send = skynet.send
+local skynet_call = skynet.call
+local skynet_pack = skynet.pack
+local skynet_ret = skynet.ret
+
+local NORET = {}
+
+local g_module_id_list_map = {}
+local g_module_watch_map = {}
+local g_module_version_map = {}
+
+local CMD = {}
+
+function CMD.load_module(source,module_name,launch_num)
+	assert(module_name,'not module_name')
+	assert(launch_num and launch_num > 0,"launch_num err")
+
+	local id_list = {}
+	for i = 1,launch_num do
+		local server_id = skynet.newservice('hot_container',module_name)
+		skynet_call(server_id,'lua','start')
+		tinsert(id_list,server_id)
+	end
+
+	local old_id_list = g_module_id_list_map[module_name] or {}
+	for _,id in ipairs(old_id_list) do
+		skynet_send(id,'lua','exit')
+	end
+
+	g_module_id_list_map[module_name] = id_list
+
+	if not g_module_version_map[module_name] then
+		g_module_version_map[module_name] = 0
+	end
+
+	if not g_module_watch_map[module_name] then
+		g_module_watch_map[module_name] = {}
+	end
+	
+	g_module_version_map[module_name] = g_module_version_map[module_name] + 1
+	local version = g_module_version_map[module_name]
+
+	local watch_map = g_module_watch_map[module_name]
+	for source,response in pairs(watch_map) do
+		response(true,id_list,version)
+		watch_map[source] = nil
+	end
+	
+	return id_list,version
+end
+
+function CMD.query(source,module_name)
+	assert(module_name,'not module_name')
+	assert(g_module_id_list_map[module_name])
+	assert(g_module_version_map[module_name])
+
+	local id_list = g_module_id_list_map[module_name]
+	local version = g_module_version_map[module_name]
+
+	return id_list,version
+end
+
+function CMD.watch(source,module_name,version)
+    assert(module_name,'not module_name')
+	assert(version,"not version")
+	assert(g_module_id_list_map[module_name])
+	assert(g_module_version_map[module_name])
+
+	local id_list = g_module_id_list_map[module_name]
+	local version = g_module_version_map[module_name]
+	local watch_map = g_module_watch_map[module_name]
+
+	assert(not watch_map[source])
+	if version ~= version then
+		return id_list,version
+	end
+
+	watch_map[source] = skynet.response()
+	return NORET
+end
+
+function CMD.unwatch(source,module_name)
+	assert(module_name,'not module_name')
+	assert(g_module_id_list_map[module_name])
+	assert(g_module_version_map[module_name])
+
+	local id_list = g_module_id_list_map[module_name]
+	local version = g_module_version_map[module_name]
+	local watch_map = g_module_watch_map[module_name]
+	local response = watch_map[source]
+	assert(response)
+
+	response(true,id_list,version)
+	watch_map[source] = nil
+	return true
+end
+
+skynet.start(function()
+	skynet.register('.contriner_mgr')
+	skynet.dispatch('lua',function(session,source,cmd,...)
+		local f = CMD[cmd]
+		assert(f,'cmd no found :'..cmd)
+		local r1,r2 = f(source,...)
+		if r1 ~= NORET then
+			skynet_ret(skynet_pack(r1,r2))
+		end
+	end)
+end)
---
+++ service/hot_container.lua
@@ -0,0 +1,46 @@
+local skynet = require "skynet"
+local cache = require "skynet.codecache"
+cache.mode "OFF"
+local assert = assert
+local tonumber = tonumber
+
+local ARGV = {...}
+local MODULE_NAME = ARGV[1]
+assert(MODULE_NAME)
+
+local CMD = require(MODULE_NAME)
+
+local module_start = CMD.start
+local module_exit = CMD.exit
+assert(module_start,MODULE_NAME .. " not start func")
+
+function CMD.start(...)
+	module_start(...)
+end
+
+function CMD.exit(list)
+	if module_exit then
+		module_exit()
+	else
+		skynet.timeout(60,function()
+			skynet.error(MODULE_NAME .. ' exit')
+		end)
+	end
+end
+
+skynet.start(function()
+	if CMD.init then
+		CMD.init()
+	end
+
+	skynet.dispatch('lua',function(session,source,cmd,...)
+		local f = CMD[cmd]
+		assert(f,'cmd no found :'..cmd)
+	
+		if session == 0 then
+			f(...)
+		else
+			skynet.retpack(f(...))
+		end
+	end)
+end)
