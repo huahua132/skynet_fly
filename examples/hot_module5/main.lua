fcf9e5282a472b3d0d9bbd8a07785304ce30eb22
Faker2 <faker@git.com>
2023-06-21 18:32:58 +0800

    skynet config 的自动化编写

--- examples/hot_module5/main.lua
+++ examples/hot_module5/main.lua
@@ -1,14 +1,17 @@
 local skynet = require "skynet"
+local util = require "util"
 
 skynet.start(function()
 	skynet.error("start hot_module5!!!>>>>>>>>>>>>>>>>>")
 	local cmgr = skynet.uniqueservice('contriner_mgr')
 
-	skynet.call(cmgr,'lua','load_module',"service_m",1,{{player_num = 2,min_num = 1,max_num = 100}})
-	skynet.call(cmgr,'lua','load_module',"agent_m",2,{
-		{player_id = 10001,nickname = "张三"},
-		{player_id = 10002,nickname = "李四"},
-	})
-	
-	skynet.call(cmgr,'lua','load_module',"service_m",1,{{player_num = 2,min_num = 100,max_num = 200}})
+	local mod_config = load(skynet.getenv('mod_config'))()
+
+	for mod_name,mod_cfg in util.sort_ipairs(mod_config,function(a,b)
+		return a.launch_seq < b.launch_seq
+	end) do
+		skynet.call(cmgr,'lua','load_module',mod_name,mod_cfg.launch_num,mod_cfg.mod_args,mod_cfg.default_arg)
+	end
+
+	skynet.exit()
 end)
