59940b0f7ec1eaed0fbe8d0127d66c746e8933a3
huahua132 <958677003@qq.com>
2023-07-02 12:47:50 +0800

    reredis—_m封装

--- test/load_mods.lua
+++ test/load_mods.lua
@@ -1,6 +1,20 @@
 return {
-	timer_test_m = {
-		launch_seq = 1,
+	-- timer_test_m = {
+	-- 	launch_seq = 1,
+	-- 	launch_num = 1,
+	-- },
+	redis_m = {
+		launch_seq = 2,
 		launch_num = 1,
-	}
+		default_arg = {
+			host = "127.0.0.1",
+			port = "6379",
+			auth = "123456",
+			db = 0,
+		},
+	},
+	redis_test_m = {
+		launch_seq = 3,
+		launch_num = 1,
+	},
 }
