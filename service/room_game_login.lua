5d54526f41856a1efd3fa4b283a785a494d604f1
huahua132 <958677003@qq.com>
2023-07-21 17:19:29 +0800

    log

--- service/room_game_login.lua
+++ service/room_game_login.lua
@@ -7,7 +7,7 @@ local contriner_client = require "contriner_client"
 local assert = assert
 local x_pcall = x_pcall
 
-local check_plug = nil
+local login_plug = nil
 local SELF_ADDRESS = nil
 
 local g_gate = nil
@@ -32,7 +32,7 @@ function CMD.goout(player_id)
 	local agent = assert(g_player_map[player_id])
 
 	g_player_map[player_id] = nil
-	check_plug.login_out(player_id)
+	login_plug.login_out(player_id)
 end
 
 local SOCKET = {}
@@ -45,7 +45,7 @@ function SOCKET.open(fd, addr,gate)
 		addr = addr,
 		gate = gate,
 		queue = queue(),
-		login_time_out = timer:new(check_plug.time_out,1,close_fd,fd)
+		login_time_out = timer:new(login_plug.time_out,1,close_fd,fd)
 	}
 	g_fd_agent_map[fd] = agent
 	skynet.send(gate,'lua','forward',fd)
@@ -67,7 +67,7 @@ function SOCKET.close(fd)
 	if player then
 		local hall_client = player.hall_client
 		hall_client:mod_send('disconnect',agent.gate,fd,player_id)
-		check_plug.disconnect(agent.gate,fd,player_id)
+		login_plug.disconnect(agent.gate,fd,player_id)
 	end
 end
 
@@ -86,7 +86,7 @@ local function connect_hall(gate,fd,player_id)
 	local hall_client = nil
 	if old_agent then
 		hall_client = old_agent.hall_client
-		check_plug.repeat_login(old_agent.gate,old_agent.fd,player_id)
+		login_plug.repeat_login(old_agent.gate,old_agent.fd,player_id)
 		close_fd(old_agent.fd)
 	else
 		hall_client = contriner_client:new("room_game_hall_m",nil,function() return false end)
@@ -95,7 +95,7 @@ local function connect_hall(gate,fd,player_id)
 	
 	local ret,errcode,errmsg = hall_client:mod_call("connect",gate,fd,player_id,SELF_ADDRESS)
 	if not ret then
-		check_plug.login_failed(gate,fd,player_id,errcode,errmsg)
+		login_plug.login_failed(gate,fd,player_id,errcode,errmsg)
 		return
 	end
 
@@ -106,20 +106,20 @@ local function connect_hall(gate,fd,player_id)
 		fd = fd,
 	}
 
-	check_plug.login_succ(gate,fd,player_id,ret)
+	login_plug.login_succ(gate,fd,player_id,ret)
 	return true
 end
 
 local function check_func(gate,fd,...)
-	local player_id,errcode,errmsg = check_plug.check(gate,fd,...)
+	local player_id,errcode,errmsg = login_plug.check(gate,fd,...)
 	if not player_id then
-		check_plug.login_failed(gate,fd,player_id,errcode,errmsg)
+		login_plug.login_failed(gate,fd,player_id,errcode,errmsg)
 		return
 	end
 
 	if g_login_lock_map[player_id] then
 		--正在登入中
-		check_plug.logining(gate,fd,player_id)
+		login_plug.logining(gate,fd,player_id)
 		return
 	end
 	
@@ -153,25 +153,25 @@ skynet.start(function()
 
 	assert(room_game_login.gateservice,"not gateservice")
 	assert(room_game_login.gateconf,"not gateconf")
-	assert(room_game_login.check_plug,"not check_plug")
+	assert(room_game_login.login_plug,"not login_plug")
 
-	check_plug = require (room_game_login.check_plug)
-	assert(check_plug.init,"check_plug not init")				   --初始化
-	assert(check_plug.unpack,"check_plug not unpack")              --解包函数
-	assert(check_plug.check,"check_plug not check")				   --登录检查
-	assert(check_plug.login_succ,"check_plug not login_succ")	   --登录成功
-	assert(check_plug.login_failed,"check_plug not login_failed")  --登录失败
-	assert(check_plug.disconnect,"check_plug not disconnect")      --掉线
-	assert(check_plug.login_out,"check_plug not login_out")        --登出
-	assert(check_plug.time_out,"check_plug not time_out")		   --登录超时时间
+	login_plug = require (room_game_login.login_plug)
+	assert(login_plug.init,"login_plug not init")				   --初始化
+	assert(login_plug.unpack,"login_plug not unpack")              --解包函数
+	assert(login_plug.check,"login_plug not check")				   --登录检查
+	assert(login_plug.login_succ,"login_plug not login_succ")	   --登录成功
+	assert(login_plug.login_failed,"login_plug not login_failed")  --登录失败
+	assert(login_plug.disconnect,"login_plug not disconnect")      --掉线
+	assert(login_plug.login_out,"login_plug not login_out")        --登出
+	assert(login_plug.time_out,"login_plug not time_out")		   --登录超时时间
 	
-	assert(check_plug.logining,"check_plug not logining")          --正在登录中
-	assert(check_plug.repeat_login,"check_plug not repeat_login")  --重复登录
+	assert(login_plug.logining,"login_plug not logining")          --正在登录中
+	assert(login_plug.repeat_login,"login_plug not repeat_login")  --重复登录
 
 	skynet.register_protocol {
 		id = skynet.PTYPE_CLIENT,
 		name = "client",
-		unpack = check_plug.unpack,
+		unpack = login_plug.unpack,
 		dispatch = function(fd,source,...)
 			skynet.ignoreret()
 			local agent = g_fd_agent_map[fd]
@@ -197,6 +197,6 @@ skynet.start(function()
 		end,
 	}
 	g_gate = skynet.newservice(room_game_login.gateservice)
-	check_plug.init()
+	login_plug.init()
 	skynet.call(g_gate,'lua','open',room_game_login.gateconf)
 end)
