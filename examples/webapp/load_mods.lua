853bc65a0055a4bf64ee15e5f7e9fd28ba632a7f
huahua132 <958677003@qq.com>
2023-08-01 17:10:40 +0800

    基准测试

--- examples/webapp/load_mods.lua
+++ examples/webapp/load_mods.lua
@@ -5,6 +5,8 @@ return {
 		default_arg = {
 			protocol = 'http',
 			dispatch = 'webapp_dispatch',
+			keep_alive_time = 300,         --最长保活时间
+			second_req_limit = 2000,       --1秒内请求数量限制
 		}
 	},
 
@@ -13,7 +15,10 @@ return {
 		launch_num = 1,
 		default_arg = {
 			protocol = 'http',
-			port = 80,
+			port = 80,         --端口
+			max_client = 2048, --最大连接数
+			second_conn_limit = 2000, --相同ip 1秒内建立连接数限制
+			keep_live_limit = 2000,  --相同ip 保持活跃数量限制
 		}
 	}
 }
