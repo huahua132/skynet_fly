0e49f656b9a90714d5e7a97aad826c5a6543f6d1
huahua132 <958677003@qq.com>
2023-08-08 10:35:31 +0800

    log support hooks

--- lualib/log.lua
+++ lualib/log.lua
@@ -6,14 +6,35 @@ local serror = skynet.error
 local debug_getinfo = debug.getinfo
 local MODULE_NAME = MODULE_NAME
 local SERVICE_NAME = SERVICE_NAME
+local pairs = pairs
+local assert = assert
+local tostring = tostring
+local tinsert = table.insert
+local ipairs = ipairs
+
+local M = {
+	INFO = 0,
+	DEBUG = 1,
+	WARN = 2,
+	ERROR = 3,
+	FATAL = 4,
+}
 
 local level_map = {
-	['info'] = 0,
-	['debug'] = 1,
-	['warn'] = 2,
-	['error'] = 3,
-	['fatal'] = 4,
+	['info'] = M.INFO,
+	['debug'] = M.DEBUG,
+	['warn'] = M.WARN,
+	['error'] = M.ERROR,
+	['fatal'] = M.FATAL,
 }
+
+--钩子函数
+local hooks = {}
+do
+	for n,v in pairs(M) do
+		hooks[v] = {}
+	end
+end
 
 local loglevel = skynet.getenv('loglevel') or 'info'
 local use_level = level_map[loglevel] or 0
@@ -41,12 +62,14 @@ local function create_log_func(level_name,is_format)
 			server_name = MODULE_NAME
 		end
 
-		log_str = sformat("[%s][%s][%s]%s",level_name,server_name,lineinfo,log_str)
-		serror(log_str)
+		serror(sformat("[%s][%s][%s]%s",level_name,server_name,lineinfo,log_str))
+
+		local log_hook = hooks[level]
+		for _,hook_func in ipairs(log_hook) do
+			hook_func(log_str)
+		end
 	end
 end
-
-local M = {}
 
 M.info = create_log_func("info")
 M.debug = create_log_func("debug")
@@ -58,5 +81,10 @@ M.debug_fmt = create_log_func("debug",true)
 M.warn_fmt = create_log_func("warn",true)
 M.error_fmt = create_log_func("error",true)
 M.fatal_fmt = create_log_func("fatal",true)
+
+function M.add_hook(loglevel,hook_func)
+	assert(hooks[loglevel],"not loglevel " .. tostring(loglevel))
+	tinsert(hooks[loglevel],hook_func)
+end
 
 return M
