f478031a6ea9aebf36916e035c4dc55ddec4fac0
huahua132 <958677003@qq.com>
2023-09-05 16:02:53 +0800

    配置logrotate不跟随run.sh执行，单独一个sh文件

--- script/lua/write_create_logrotate.lua
+++ script/lua/write_create_logrotate.lua
@@ -13,8 +13,7 @@ local server_path = "./"
 local lua_path = skynet_path .. '/3rd/lua/lua'
 
 local shell_str = "#!bin/bash\n"
-shell_str = shell_str .. string.format("%s/skynet %s_config.lua\n",skynet_path,svr_name)
-shell_str = shell_str .. string.format("%s %s/script/lua/console.lua %s %s create_mod_config_old\n",lua_path,skynet_fly_path,skynet_fly_path,svr_name)
+shell_str = shell_str .. string.format("%s %s/script/lua/console.lua %s %s create_logrotate\n",lua_path,skynet_fly_path,skynet_fly_path,svr_name)
 
 local shell_path = server_path .. '/script/'
 
@@ -22,7 +21,7 @@ if not os.execute("mkdir -p " .. shell_path) then
 	error("create shell_path err")
 end
 
-local file_path = shell_path .. 'run.sh'
+local file_path = shell_path .. 'create_logrotate.sh'
 
 local file = io.open(file_path,'w+')
 assert(file)
