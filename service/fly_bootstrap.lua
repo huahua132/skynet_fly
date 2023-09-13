bc857d424371d7faf219ec9e9e9628f3788afc28
huahua132 <958677003@qq.com>
2023-09-13 11:49:57 +0800

    fly_bootstrap优化

--- service/fly_bootstrap.lua
+++ service/fly_bootstrap.lua
@@ -12,51 +12,5 @@ skynet.start(function()
 		env_util.add_pre_load(preload:sub(1,preload:len() - 1))
 	end
 
-	local standalone = skynet.getenv "standalone"
-
-	local launcher = assert(skynet.launch("snlua","launcher"))
-	skynet.name(".launcher", launcher)
-
-	local harbor_id = tonumber(skynet.getenv "harbor" or 0)
-	if harbor_id == 0 then
-		assert(standalone ==  nil)
-		standalone = true
-		skynet.setenv("standalone", "true")
-
-		local ok, slave = pcall(skynet.newservice, "cdummy")
-		if not ok then
-			skynet.abort()
-		end
-		skynet.name(".cslave", slave)
-
-	else
-		if standalone then
-			if not pcall(skynet.newservice,"cmaster") then
-				skynet.abort()
-			end
-		end
-
-		local ok, slave = pcall(skynet.newservice, "cslave")
-		if not ok then
-			skynet.abort()
-		end
-		skynet.name(".cslave", slave)
-	end
-
-	if standalone then
-		local datacenter = skynet.newservice "datacenterd"
-		skynet.name("DATACENTER", datacenter)
-	end
-	skynet.newservice "service_mgr"
-
-	local enablessl = skynet.getenv "enablessl"
-	if enablessl then
-		service.new("ltls_holder", function ()
-			local c = require "ltls.init.c"
-			c.constructor()
-		end)
-	end
-
-	pcall(skynet.newservice,skynet.getenv "start" or "main")
-	skynet.exit()
+	skynet.launch("snlua","bootstrap")
 end)
