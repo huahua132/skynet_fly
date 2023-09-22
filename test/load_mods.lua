225a2b6a3c965cb42e80ee28cb86d567b6a1ddb4
huahua132 <958677003@qq.com>
2023-09-22 15:12:16 +0800

    field_check_help

--- test/load_mods.lua
+++ test/load_mods.lua
@@ -30,61 +30,61 @@ return {
 	-- 	launch_num = 1,
 	-- },
 
-	mysql_m = {
-		launch_seq = 4,
-		launch_num = 4,
-		mod_args = {
-			{
-				instance_name = "game",
-				db_conf = {
-					host = '127.0.0.1',
-					port = '3306',
-					max_packet_size = 1048576,
-					user = 'root',
-					password = '123456',
-					database = 'gamedb',
-				}
-			},
-			{
-				instance_name = "game",
-				db_conf = {
-					host = '127.0.0.1',
-					port = '3306',
-					max_packet_size = 1048576,
-					user = 'root',
-					password = '123456',
-					database = 'gamedb',
-				}
-			},
-			{
-				instance_name = "hall",
-				db_conf = {
-					host = '127.0.0.1',
-					port = '3306',
-					max_packet_size = 1048576,
-					user = 'root',
-					password = '123456',
-					database = 'halldb',
-				}
-			},
-			{
-				instance_name = "hall",
-				db_conf = {
-					host = '127.0.0.1',
-					port = '3306',
-					max_packet_size = 1048576,
-					user = 'root',
-					password = '123456',
-					database = 'halldb',
-				}
-			},
-		}
-	},
+	-- mysql_m = {
+	-- 	launch_seq = 4,
+	-- 	launch_num = 4,
+	-- 	mod_args = {
+	-- 		{
+	-- 			instance_name = "game",
+	-- 			db_conf = {
+	-- 				host = '127.0.0.1',
+	-- 				port = '3306',
+	-- 				max_packet_size = 1048576,
+	-- 				user = 'root',
+	-- 				password = '123456',
+	-- 				database = 'gamedb',
+	-- 			}
+	-- 		},
+	-- 		{
+	-- 			instance_name = "game",
+	-- 			db_conf = {
+	-- 				host = '127.0.0.1',
+	-- 				port = '3306',
+	-- 				max_packet_size = 1048576,
+	-- 				user = 'root',
+	-- 				password = '123456',
+	-- 				database = 'gamedb',
+	-- 			}
+	-- 		},
+	-- 		{
+	-- 			instance_name = "hall",
+	-- 			db_conf = {
+	-- 				host = '127.0.0.1',
+	-- 				port = '3306',
+	-- 				max_packet_size = 1048576,
+	-- 				user = 'root',
+	-- 				password = '123456',
+	-- 				database = 'halldb',
+	-- 			}
+	-- 		},
+	-- 		{
+	-- 			instance_name = "hall",
+	-- 			db_conf = {
+	-- 				host = '127.0.0.1',
+	-- 				port = '3306',
+	-- 				max_packet_size = 1048576,
+	-- 				user = 'root',
+	-- 				password = '123456',
+	-- 				database = 'halldb',
+	-- 			}
+	-- 		},
+	-- 	}
+	-- },
 
-	mysql_test_m = {
-		launch_seq = 5,
-		launch_num = 1,
-	},
+	-- mysql_test_m = {
+	-- 	launch_seq = 5,
+	-- 	launch_num = 1,
+	-- },
 
 	-- proto_test_m = {
 	-- 	launch_seq = 6,
@@ -114,4 +114,8 @@ return {
 	-- 	launch_seq = 10,
 	-- 	launch_num = 1,
 	-- }
+	filed_check_test_m = {
+		launch_seq = 11,
+		launch_num = 1,
+	}
 }
