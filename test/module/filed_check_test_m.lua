edae348a00db6a4c6f8cacdaba8e01a5550c6f75
huahua132 <958677003@qq.com>
2023-09-22 15:23:10 +0800

    filed_check_test_m

--- test/module/filed_check_test_m.lua
+++ test/module/filed_check_test_m.lua
@@ -33,6 +33,21 @@ local cfg_struct = {
                 _begin_func = is_number,
             }
         },
+
+        cfg_list = {
+            _begin_func = is_table,
+            _repeat_field = {
+                _begin_func = is_table,
+                _map_field = {
+                    min_bet = {
+                        _begin_func = is_number,
+                    },
+                    max_bet = {
+                        _begin_func = is_number,
+                    },
+                }
+            }
+        }
     }
 }
 
@@ -47,6 +62,9 @@ local function test_map_add()
 
         player_list = {
             1,2,35,6,
+        },
+        cfg_list = {
+            {min_bet = 1,max_bet = 2},
         }
     }
 
@@ -61,6 +79,9 @@ local function test_map_add()
 
         player_list = {
             1,2,35,6,
+        },
+        cfg_list = {
+            {min_bet = 1,max_bet = 2},
         }
     }
 
@@ -83,6 +104,9 @@ local function test_map_reduce()
 
         player_list = {
             1,2,35,6,
+        },
+        cfg_list = {
+            {min_bet = 1,max_bet = 2},
         }
     }
 
@@ -96,6 +120,9 @@ local function test_map_reduce()
 
         player_list = {
             1,2,35,6,
+        },
+        cfg_list = {
+            {min_bet = 1,max_bet = 2},
         }
     }
     local _,check_map = field_check_help.check_args(new_t,cfg_struct,nil,'root')
@@ -116,6 +143,9 @@ local function test_list_add()
 
         player_list = {
             1,2,35,6,
+        },
+        cfg_list = {
+            {min_bet = 1,max_bet = 2},
         }
     }
 
@@ -129,6 +159,9 @@ local function test_list_add()
 
         player_list = {
             1,2,35,6,100,
+        },
+        cfg_list = {
+            {min_bet = 1,max_bet = 2},
         }
     }
 
@@ -150,6 +183,9 @@ local function test_list_reduce()
 
         player_list = {
             1,2,35,6,
+        },
+        cfg_list = {
+            {min_bet = 1,max_bet = 2},
         }
     }
 
@@ -163,6 +199,9 @@ local function test_list_reduce()
 
         player_list = {
             1,2,35,
+        },
+        cfg_list = {
+            {min_bet = 1,max_bet = 2},
         }
     }
 
@@ -184,6 +223,9 @@ local function keep_update_test_up()
 
         player_list = {
             1,2,35,
+        },
+        cfg_list = {
+            {min_bet = 1,max_bet = 2},
         }
     }
 
@@ -197,6 +239,9 @@ local function keep_update_test_up()
 
         player_list = {
             1,2,35,
+        },
+        cfg_list = {
+            {min_bet = 1,max_bet = 2},
         }
     }
 
@@ -218,6 +263,9 @@ local function keep_update_test_not_up()
 
         player_list = {
             1,2,35,
+        },
+        cfg_list = {
+            {min_bet = 1,max_bet = 2},
         }
     }
 
@@ -231,6 +279,9 @@ local function keep_update_test_not_up()
 
         player_list = {
             1,2,35,
+        },
+        cfg_list = {
+            {min_bet = 1,max_bet = 2},
         }
     }
 
@@ -238,6 +289,88 @@ local function keep_update_test_not_up()
     local is_update = field_check_help.update_args(old_t,new_t,check_map,cfg_struct)
     assert(old_t.min_limit == 1)
     log.info("keep_update_test_not_up:",old_t)
+    return is_update
+end
+
+local function test_repeat_table_add_test()
+    local old_t = {
+        min_limit = 1,
+        max_limit = 2,
+
+        player_map = {
+            [10001] = 1,
+        },
+
+        player_list = {
+            1,2,35,
+        },
+        cfg_list = {
+            {min_bet = 1,max_bet = 2},
+        }
+    }
+
+    local new_t = {
+        min_limit = 1,
+        max_limit = 2,
+
+        player_map = {
+            [10001] = 1,
+        },
+
+        player_list = {
+            1,2,35,
+        },
+        cfg_list = {
+            {min_bet = 1,max_bet = 2},
+            {min_bet = 3,max_bet = 4},
+        }
+    }
+
+    local _,check_map = field_check_help.check_args(new_t,cfg_struct,nil,'root')
+    local is_update = field_check_help.update_args(old_t,new_t,check_map,cfg_struct)
+    assert(old_t.cfg_list[2].min_bet == 3)
+    log.info("test_repeat_table_add_test:",old_t)
+    return is_update
+end
+
+local function test_repeat_table_reduce_test()
+    local old_t = {
+        min_limit = 1,
+        max_limit = 2,
+
+        player_map = {
+            [10001] = 1,
+        },
+
+        player_list = {
+            1,2,35,
+        },
+        cfg_list = {
+            {min_bet = 1,max_bet = 2},
+            {min_bet = 3,max_bet = 4},
+        }
+    }
+
+    local new_t = {
+        min_limit = 1,
+        max_limit = 2,
+
+        player_map = {
+            [10001] = 1,
+        },
+
+        player_list = {
+            1,2,35,
+        },
+        cfg_list = {
+            {min_bet = 1,max_bet = 2},
+        }
+    }
+
+    local _,check_map = field_check_help.check_args(new_t,cfg_struct,nil,'root')
+    local is_update = field_check_help.update_args(old_t,new_t,check_map,cfg_struct)
+    assert(old_t.cfg_list[2] == nil)
+    log.info("test_repeat_table_reduce_test:",old_t)
     return is_update
 end
 
@@ -250,6 +383,8 @@ function CMD.start()
     assert(test_list_reduce())
     assert(keep_update_test_up())
     assert(keep_update_test_not_up())
+    assert(test_repeat_table_add_test())
+    assert(test_repeat_table_reduce_test())
     return true
 end
 
