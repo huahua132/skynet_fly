9de1ae32b9861274b6ebf7475cb8cf5727add292
huahua132 <958677003@qq.com>
2023-10-21 16:13:23 +0800

    整点报时定时器

--- lualib/time_extend/timer_point.lua
+++ lualib/time_extend/timer_point.lua
@@ -2,12 +2,13 @@
 local skynet = require "skynet"
 local timer = require "timer"
 local time_util = require "time_util"
-local log = require "log"
 
 local tostring = tostring
 local assert = assert
 local pairs = pairs
 local setmetatable = setmetatable
+local rawget = rawget
+local rawset = rawset
 
 local M = {
     --每分钟
@@ -16,6 +17,14 @@ local M = {
     EVERY_HOUR = 2,
     --每天
     EVERY_DAY = 3,
+    --每周
+    EVERY_WEEK = 4,
+    --每月 超过适配到最后一天
+    EVERY_MOUTH = 5,
+    --每年几月几日几时几分几秒 超过适配到最后一天
+    EVERY_YEAR = 6,
+    --每年的第几天 超过适配到最后一天
+    EVERY_YEAR_DAY = 7,
 }
 
 local g_type_map = {}
@@ -26,54 +35,150 @@ do
 end
 
 local TYPE_REMAIN_TIME_FUNC = {
-    [M.EVERY_MINUTE] = function()
+    [M.EVERY_MINUTE] = function(month,day,hour,min,sec,wday,yday)
         local cur_time = time_util.time()
-        local next_time = time_util.next_min()
+        local next_time = time_util.next_min(sec)
         return next_time - cur_time
     end,
-    [M.EVERY_HOUR] = function()
+    [M.EVERY_HOUR] = function(month,day,hour,min,sec,wday,yday)
         local cur_time = time_util.time()
-        local next_time = time_util.next_hour()
+        local next_time = time_util.next_hour(min,sec)
         return next_time - cur_time
     end,
-    [M.EVERY_DAY] = function()
+    [M.EVERY_DAY] = function(month,day,hour,min,sec,wday,yday)
         local cur_time = time_util.time()
-        local next_time = time_util.next_day()
+        local next_time = time_util.next_day(hour,min,sec)
         return next_time - cur_time
     end,
+    [M.EVERY_WEEK] = function(month,day,hour,min,sec,wday,yday)
+        local cur_time = time_util.time()
+        local next_time = time_util.next_week(wday,hour,min,sec)
+        return next_time - cur_time
+    end,
+    [M.EVERY_MOUTH] = function(month,day,hour,min,sec,wday,yday)
+        local cur_time = time_util.time()
+        local next_time = time_util.next_month(day,hour,min,sec)
+        return next_time - cur_time
+    end,
+    [M.EVERY_YEAR] = function(month,day,hour,min,sec,wday,yday)
+        local cur_time = time_util.time()
+        local next_time = time_util.next_year(month,day,hour,min,sec)
+        return next_time - cur_time
+    end,
+    [M.EVERY_YEAR_DAY] = function(month,day,hour,min,sec,wday,yday)
+        local cur_time = time_util.time()
+        local next_time = time_util.next_year_day(yday,hour,min,sec)
+        return next_time - cur_time
+    end
 }
 
 local TYPE_INVAL_TIME_MAP = {
     [M.EVERY_MINUTE] = timer.minute,
     [M.EVERY_HOUR] = timer.hour,
     [M.EVERY_DAY] = timer.day,
+    [M.EVERY_WEEK] = timer.day * 7,
 }
 
 local mata = {
     __index = function(tb, k)
-        if M[k] then
-            return M[k]
+        local v = rawget(M,k)
+        if v then
+            return v
         end
         return tb.time_obj[k]
     end,
     __newindex = function (tb,k,v)
-        tb.time_obj[k] = v
+        local v = rawget(tb,k)
+        if v then
+            rawset(tb,k,v)
+        else
+            tb.time_obj[k] = v
+        end
     end
 }
 
-local function adapter(point_obj,type,call_back,...)
+local function adapter(point_obj,call_back,...)
     skynet.fork(call_back,...)
-    local inval_time = TYPE_INVAL_TIME_MAP[type]
+    local inval_time = TYPE_INVAL_TIME_MAP[point_obj.type]
     point_obj.time_obj = timer:new(inval_time,timer.loop,call_back,...)
 end
 
-function M:new(type,call_back,...)
+local adapter_loop
+adapter_loop = function(point_obj,call_back,...)
+    skynet.fork(call_back,...)
+    local remain_time = TYPE_REMAIN_TIME_FUNC[self.type](self.month,self.day,self.hour,self.min,self.sec,self.wday,self.yday)
+    self.time_obj = timer:new(remain_time * timer.second,1,adapter_loop,self,call_back,...)
+end
+
+function M:new(type)
     assert(g_type_map[type], "unkown type ".. tostring(type))
-    local remain_time = TYPE_REMAIN_TIME_FUNC[type]()
-    local t = {}
-    t.time_obj = timer:new(remain_time * timer.second,1,adapter,t,type,call_back,...)
+    local t = {
+        type = type,
+        month = 1,
+        day = 1,
+        hour = 0,
+        min = 0,
+        sec = 0,
+        wday = 1,
+        yday = 1,
+        time_obj = {},
+    }
     setmetatable(t,mata)
     return t
+end
+
+function M:set_month(month)
+    assert(month >= 1 and month <= 12)
+    self.month = month
+    return self
+end
+
+function M:set_day(day)
+    assert(day >= 1 and day <= 31)
+    self.day = day
+    return self
+end
+
+function M:set_hour(hour)
+    assert(hour >= 0 and hour <= 23)
+    self.hour = hour
+    return self
+end
+
+function M:set_min(min)
+    assert(min >= 0 and min <= 59)
+    self.min = min
+    return self
+end
+
+function M:set_sec(sec)
+    assert(sec >= 0 and sec <= 59)
+    self.sec = sec
+    return self
+end
+
+--星期天为 1
+function M:set_wday(wday)
+    assert(wday >= 1 and wday <= 7)
+    self.wday = wday
+    return self
+end
+
+--一年的第几天
+function M:set_yday(yday)
+    assert(yday >= 1 and yday <= 366)
+    self.yday = yday
+    return self
+end
+
+function M:builder(call_back, ...)
+    local remain_time = TYPE_REMAIN_TIME_FUNC[self.type](self.month,self.day,self.hour,self.min,self.sec,self.wday,self.yday)
+    if not TYPE_INVAL_TIME_MAP[self.type] then
+        self.time_obj = timer:new(remain_time * timer.second,1,adapter_loop,self,call_back,...)
+    else
+        self.time_obj = timer:new(remain_time * timer.second,1,adapter,self,call_back,...)
+    end
+    return self
 end
 
 return M
--- lualib/utils/time_util.lua
+++ lualib/utils/time_util.lua
@@ -37,9 +37,9 @@ end
 --明天 0点0分0秒 就是 1，0，0，0
 function M.day_time(day,hour,min,sec)
 	assert(day)
-	assert(hour >= 0 and hour <= 24,hour)
-	assert(min >= 0 and min <= 60,min)
-	assert(sec >= 0 and sec <= 60,sec)
+	assert(hour >= 0 and hour <= 23,hour)
+	assert(min >= 0 and min <= 59,min)
+	assert(sec >= 0 and sec <= 59,sec)
   
 	local sub_day_time = day * 86400
 	local date = os.date("*t",M.time() + sub_day_time)
@@ -49,31 +49,148 @@ function M.day_time(day,hour,min,sec)
 	return os.time(date)
 end
 
---下一分钟的时间戳
-function M.next_min()
+--下一分钟的第几秒时间戳
+function M.next_min(sec)
+	assert(sec >= 0 and sec <= 59)
 	local cur_date = M.date()
-	cur_date.sec = 0
+	cur_date.sec = sec
 	local pre_time = os.time(cur_date)
 	return pre_time + M.MINUTE
 end
 
---下一小时的时间戳
-function M.next_hour()
+--下一小时的第几分钟第几秒
+function M.next_hour(min,sec)
+	assert(sec >= 0 and sec <= 59)
+	assert(min >= 0 and min <= 59)
 	local cur_date = M.date()
-	cur_date.sec = 0
-	cur_date.min = 0
+	cur_date.sec = sec
+	cur_date.min = min
 	local pre_time = os.time(cur_date)
 	return pre_time + M.HOUR
 end
 
---下一天的时间戳
-function M.next_day()
+--下一天的几点几分几秒
+function M.next_day(hour,min,sec)
+	assert(hour >= 0 and hour <= 23,hour)
+	assert(min >= 0 and min <= 59,min)
+	assert(sec >= 0 and sec <= 59,sec)
 	local cur_date = M.date()
-	cur_date.sec = 0
-	cur_date.min = 0
-	cur_date.hour = 0
+	cur_date.sec = sec
+	cur_date.min = min
+	cur_date.hour = hour
 	local pre_time = os.time(cur_date)
 	return pre_time + M.DAY
+end
+
+--下一周的周几几点几分几秒
+function M.next_week(wday,hour,min,sec)
+	assert(wday >= 1 and wday <= 7)
+	assert(hour >= 0 and hour <= 23,hour)
+	assert(min >= 0 and min <= 59,min)
+	assert(sec >= 0 and sec <= 59,sec)
+	local cur_date = M.date()
+	cur_date.sec = sec
+	cur_date.min = min
+	cur_date.hour = hour
+	cur_date.day = cur_date.day + 1
+	os.date("*t",os.time(cur_date))
+	for i = 1,6 do
+		if cur_date.wday == wday then
+			break
+		end
+		cur_date.day = cur_date.day + 1
+		os.date("*t",os.time(cur_date))
+	end
+	return os.time(cur_date)
+end
+
+--下个月的第几天几时几分几秒
+--如果单月没有该天，适配到最后一天
+function M.next_month(day,hour,min,sec)
+	assert(day >= 1 and day <= 31)
+	assert(hour >= 0 and hour < 24,hour)
+	assert(min >= 0 and min < 60,min)
+	assert(sec >= 0 and sec < 60,sec)
+
+	local cur_date = M.date()
+	local next_month = cur_date.month + 1  --直接加没事，os.time会适配到下一年
+	cur_date.month = next_month
+	cur_date.day = day
+	cur_date.hour = hour
+	cur_date.min = min
+	cur_date.sec = sec
+	
+	os.date("*t",os.time(cur_date))
+	while cur_date.day ~= day do
+		day = day - 1
+		cur_date.month = next_month
+		cur_date.day = day
+		cur_date.hour = hour
+		cur_date.min = min
+		cur_date.sec = sec
+		os.date("*t",os.time(cur_date))
+	end
+	return os.time(cur_date)
+end
+
+--下一年的第几月第几天几时几分几秒
+--如果单月没有该天，适配到最后一天
+function M.next_year(month,day,hour,min,sec)
+	assert(month >= 1 and month <= 12)
+	assert(day >= 1 and day <= 31)
+	assert(hour >= 0 and hour < 24,hour)
+	assert(min >= 0 and min < 60,min)
+	assert(sec >= 0 and sec < 60,sec)
+
+	local cur_date = M.date()
+	cur_date.year = cur_date.year + 1
+	cur_date.month = month
+	cur_date.day = day
+	cur_date.hour = hour
+	cur_date.min = min
+	cur_date.sec = sec
+	os.date("*t",os.time(cur_date))
+	while cur_date.day ~= day do
+		day = day - 1
+		cur_date.month = month
+		cur_date.day = day
+		cur_date.hour = hour
+		cur_date.min = min
+		cur_date.sec = sec
+		os.date("*t",os.time(cur_date))
+	end
+	return os.time(cur_date)
+end
+
+--下一年的第几天几时几分几秒
+--如果单月没有该天，适配到最后一天
+function M.next_year_day(yday,hour,min,sec)
+	assert(yday >= 1 and yday <= 366)
+	assert(hour >= 0 and hour < 24,hour)
+	assert(min >= 0 and min < 60,min)
+	assert(sec >= 0 and sec < 60,sec)
+
+	local cur_date = M.date()
+	cur_date.year = cur_date.year + 1
+	local next_year = cur_date.year
+	cur_date.month = 1
+	cur_date.day = 1
+	cur_date.hour = hour
+	cur_date.min = min
+	cur_date.sec = sec
+	os.date("*t",os.time(cur_date))
+	for i = 1,365 do
+		if cur_date.yday == yday then
+			break
+		end
+		cur_date.day = cur_date.day + 1
+		os.date("*t",os.time(cur_date))
+		if cur_date.year > next_year then
+			cur_date.day = cur_date.day - 1
+			break
+		end
+	end
+	return os.time(cur_date)
 end
 
 return M
