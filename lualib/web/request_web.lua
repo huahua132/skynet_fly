2d991721d31e7d5a09fb266528ac29e31f87efaa
huahua132 <958677003@qq.com>
2023-11-01 11:01:00 +0800

    web

--- lualib/web/request_web.lua
+++ lualib/web/request_web.lua
@@ -12,10 +12,6 @@ local parse_url = urllib.parse
 local M = {}
 local mt = { __index = M }
 
-local PARSE_BODY_FUNC = {}
-PARSE_BODY_FUNC['application/x-www-form-urlencoded'] = parse_query
-PARSE_BODY_FUNC['application/json'] = json.decode
-
 -- new request: init args/body etc from http request
 function M:new(req)
 	local header = req.header
@@ -24,7 +20,12 @@ function M:new(req)
     local content_type = header['content-type']
     -- the post request have Content-Type header set
 
-	local parse_func = PARSE_BODY_FUNC[content_type]
+	local parse_func = nil
+    if sfind(content_type, 'application/x-www-form-urlencoded', nil, true) then
+        parse_func = parse_query
+    elseif sfind(content_type, 'application/json', nil, true) then
+        parse_func = json.decode
+    end
     if parse_func then
         body = parse_func(body)
     -- the post request have no Content-Type header set will be parsed as x-www-form-urlencoded by default
