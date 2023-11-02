70d1d5470f2117b774c43288d933f46f894fac48
huahua132 <958677003@qq.com>
2023-11-02 09:44:53 +0800

    mysql

--- lualib/db/mysqlf.lua
+++ lualib/db/mysqlf.lua
@@ -7,6 +7,7 @@ local setmetatable = setmetatable
 
 contriner_client:register("mysql_m")
 
+local g_instance = nil
 local g_instance_map = {}
 
 local M = {}
@@ -23,6 +24,11 @@ function M:new(db_name)
 end
 
 function M:instance(db_name)
+	if not db_name then
+		g_instance = g_instance or M:new()
+		return g_instance
+	end
+
 	if not g_instance_map[db_name] then
 		g_instance_map[db_name] = M:new(db_name)
 	end
