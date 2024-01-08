924358d8c477ff2ea6145b10481fe38e60daa5c1
fred <474344392@qq.com>
2024-01-08 18:46:01 +0800

    配置信息

--- script/lua/write_config.lua
+++ script/lua/write_config.lua
@@ -1,10 +1,11 @@
 local assert = assert
-local ARGV = {...}
+local ARGV = { ... }
 local skynet_fly_path = ARGV[1]
-assert(skynet_fly_path,'缺少 skynet_fly_path')
+local load_mods_name = ARGV[2]
+assert(skynet_fly_path, '缺少 skynet_fly_path')
 
 package.cpath = skynet_fly_path .. "/luaclib/?.so;"
-package.path = './?.lua;' .. skynet_fly_path .."/lualib/utils/?.lua;"
+package.path = './?.lua;' .. skynet_fly_path .. "/lualib/utils/?.lua;"
 
 local lfs = require "lfs"
 local json = require "cjson"
@@ -17,57 +18,82 @@ local common_path = "../../common/"
 
 local svr_name = file_util.get_cur_dir_name()
 local config = {
-	thread = 4,
-	start = "main",
-	harbor = 0,
-	profile = true,
-	lualoader	= skynet_fly_path.."/lualib/loader.lua",
-	bootstrap 	= "snlua fly_bootstrap",        --the service for fly_bootstrap
-	logger 		= "log_service",
-	loglevel    = "info",
-	logpath		= server_path .. 'logs/',
-	logfilename = 'server.log',
-	logservice = 'snlua',
-	daemon	= "./skynet.pid",
-	svr_id = 1,
-	svr_name = svr_name,
-	debug_port = 8888,
+	thread          = 4,
+	start           = "main",
+	harbor          = 0,
+	profile         = true,
+	lualoader       = skynet_fly_path .. "/lualib/loader.lua",
+	bootstrap       = "snlua fly_bootstrap", --the service for fly_bootstrap
+	logger          = "log_service",
+	loglevel        = "info",
+	logpath         = server_path .. 'logs/',
+	logfilename     = 'server.log',
+	logservice      = 'snlua',
+	daemon          = "./skynet.pid",
+	svr_id          = 1,
+	svr_name        = svr_name,
+	debug_port      = 8888,
 	skynet_fly_path = skynet_fly_path,
-	preload = skynet_fly_path .. '/lualib/preload.lua;',
-	cpath = skynet_fly_path .. "/cservice/?.so;" .. skynet_path .. "cservice/?.so;",
+	preload         = skynet_fly_path .. '/lualib/preload.lua;',
+	cpath           = skynet_fly_path .. "/cservice/?.so;" .. skynet_path .. "cservice/?.so;",
 
-	lua_cpath = skynet_fly_path .. "/luaclib/?.so;" .. skynet_path .. "luaclib/?.so;",
+	lua_cpath       = skynet_fly_path .. "/luaclib/?.so;" .. skynet_path .. "luaclib/?.so;",
 
 	--luaservice 约束服务只能放在 server根目录 || server->service || common->service || skynet_fly->service || skynet->service
-	luaservice = server_path .. "?.lua;" .. 
- 			 server_path .. "service/?.lua;" .. 
-			  skynet_fly_path .. "/service/?.lua;" .. 
-			  common_path .. "service/?.lua;" ..
-			 skynet_path .. "service/?.lua;",
+	luaservice      = server_path .. "?.lua;" ..
+		server_path .. "service/?.lua;" ..
+		skynet_fly_path .. "/service/?.lua;" ..
+		common_path .. "service/?.lua;" ..
+		skynet_path .. "service/?.lua;",
 
-	lua_path = "",
-	enablessl = true,
-	loadmodsfile = "load_mods",      --可热更服务启动配置
+	lua_path        = "",
+	enablessl       = true,
+	loadmodsfile    = load_mods_name, --可热更服务启动配置
 }
 
 config.lua_path = file_util.create_luapath(skynet_fly_path)
 
+local load_mods_f = require(config.loadmodsfile)
+if load_mods_f.share_config_m and load_mods_f.share_config_m.default_arg and load_mods_f.share_config_m.default_arg.server_cfg then
+	local cfg = load_mods_f.share_config_m.default_arg.server_cfg
+	if cfg.svr_id then
+		config.svr_id = cfg.svr_id
+	end
+	if cfg.thread then
+		config.thread = cfg.thread
+	end
+	if cfg.logpath then
+		config.logpath = cfg.logpath
+	end
+	if cfg.loglevel then
+		config.loglevel = cfg.loglevel
+	end
+	if cfg.logfilename then
+		config.logfilename = cfg.logfilename
+	end
+	if cfg.debug_port then
+		config.debug_port = cfg.debug_port
+	end
+end
+
+
 local config_path = server_path .. svr_name .. '_config.lua'
+
 
 local old_config = loadfile(config_path)
 
 if old_config then
 	old_config = old_config()
-	for k,_ in pairs(config) do
+	for k, _ in pairs(config) do
 		if _G[k] then
 			config[k] = _G[k]
 		end
 	end
 end
 
-local file = io.open(config_path,'w+')
+local file = io.open(config_path, 'w+')
 assert(file)
-local str = table_util.table_to_luafile("G",config)
+local str = table_util.table_to_luafile("G", config)
 file:write(str)
 file:close()
 print("make " .. config_path)
--- script/shell/make_server.sh
+++ script/shell/make_server.sh
@@ -2,6 +2,10 @@
 #构建服务设施 skynet.config 还有load_mods run.sh
 
 skynet_fly_path=$1
+load_mods_name="load_mods"
+if [ -n "$2" ]; then
+	load_mods_name="$2"
+fi
 
 if [[ -z ${skynet_fly_path} ]]; then
 	echo "缺少 skynet_fly_path"
@@ -11,7 +15,7 @@ fi
 lua=""${skynet_fly_path}"/skynet/3rd/lua/lua"
 script_path="${skynet_fly_path}/script/lua"
 
-${lua} ${script_path}/write_config.lua ${skynet_fly_path}
+${lua} ${script_path}/write_config.lua ${skynet_fly_path} ${load_mods_name}
 ${lua} ${script_path}/write_runsh.lua ${skynet_fly_path}
 ${lua} ${script_path}/write_reloadsh.lua ${skynet_fly_path}
 ${lua} ${script_path}/write_check_reloadsh.lua ${skynet_fly_path}
