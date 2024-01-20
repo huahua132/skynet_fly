de54bd51b30c7172e3498474da917f3fda8c997e
huahua132 <958677003@qq.com>
2024-01-20 14:15:19 +0800

    readmin

--- lualib/module/orm_table_m.lua
+++ lualib/module/orm_table_m.lua
@@ -1,10 +1,31 @@
 local skynet = require "skynet"
 local queue = require "skynet.queue"()
 local assert = assert
+local pairs = pairs
+local type = type
 
 local g_orm_plug = nil
 local g_orm_obj = nil
 local G_ISCLOSE = false
+
+local g_handle = {}
+
+--------------------常用handle定义------------------
+--批量创建
+function g_handle.batch_create(...)
+    local entry_list = g_orm_obj:create_entry(...)
+    local data_list = {}
+    for i = 1,#entry_list do
+        local entry = entry_list[i]
+        data_list[i] = entry:get_entry_data()
+    end
+    return data_list
+end
+
+--创建单个
+function g_handle.create_one(entry_data)
+    
+end
 
 local CMD = {}
 
@@ -12,8 +33,14 @@ function CMD.start(config)
     assert(config.orm_plug)
 
     g_orm_plug = require(config.orm_plug)
-    assert(g_orm_plug.init, "not init")    --初始化 
-    assert(g_orm_plug.call, "not call")    --调用
+    assert(g_orm_plug.init, "not init")        --初始化 
+    assert(g_orm_plug.handle, "not handle")    --自定义处理函数
+
+    for k,func in pairs(g_orm_plug.handle) do
+        assert(type(func) == 'function', "handle k not is function:" .. k)
+        assert(g_handle[k], "handle k is exists function:" .. k)
+        g_handle[k] = func
+    end
 
     skynet.fork(function ()
         g_orm_obj = queue(g_orm_plug.init)
@@ -26,7 +53,9 @@ function CMD.call(func_name,...)
         return true
     end
 
-    return false, queue(g_orm_plug.call, func_name, ...)
+    local func = assert(g_handle[func_name], "func_name not exists:" .. func_name)
+
+    return false, queue(func, ...)
 end
 
 function CMD.herald_exit()
