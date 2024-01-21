1dae945c3e95ac0209c97b90dd505921f7d00eed
huahua132 <958677003@qq.com>
2024-01-21 15:02:51 +0800

    mysql_test

--- test/module/mysql_test_m.lua
+++ test/module/mysql_test_m.lua
@@ -9,6 +9,16 @@ local CMD = {}
 local function test()
 	log.error("mysql_test_m start!!!")
 	local game_client = contriner_client:new("mysql_m","game")
+
+	local create_sql = [[
+		CREATE TABLE IF NOT EXISTS `user` (
+			`id` int(11) NOT NULL,
+			`name` varchar(255) DEFAULT NULL,
+			PRIMARY KEY (`id`)
+		) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+	]]
+	game_client:mod_call_by_name("query", create_sql)
+
 	local ret = game_client:mod_call_by_name("query","show tables;")
 	log.error("ret :",ret)
 
@@ -19,10 +29,14 @@ local function test()
 	log.info("game select:",game_client:balance_call_by_name("query",sql_str))
 
 	local hall_client = contriner_client:new("mysql_m","hall")
+	hall_client:mod_call_by_name("query", create_sql)
 	log.info("hall select:",hall_client:balance_call_by_name("query",sql_str))
 
 	local fcli = mysqlf:new("hall")
 	log.info("fcli hall select:",fcli:query(sql_str))
+
+	mysqlf:instance("game"):query("drop table if exists user")
+	mysqlf:instance("hall"):query("drop table if exists user")
 
 	log.error("mysql_test_m over!!!")
 
