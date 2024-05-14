6e12ab0f0b5e7981f021d908c892dab15deb6317
huahua132 <958677003@qq.com>
2024-05-14 10:06:46 +0800

    删除cluster_rpc方式

--- examples/cluster_client_byredis/main.lua
+++
@@ -1,9 +0,0 @@
-local skynet = require "skynet"
-local contriner_launcher = require "skynet-fly.contriner.contriner_launcher"
-
-skynet.start(function()
-	skynet.error("start cluster_client_byredis!!!>>>>>>>>>>>>>>>>>")
-	contriner_launcher.run()
-	
-	skynet.exit()
-end)
