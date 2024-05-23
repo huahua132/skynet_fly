07d51ce76298202e4f3388b79b47d169af937599
huahua132 <958677003@qq.com>
2024-05-23 14:29:01 +0800

    readme 优化

--- examples/webapp/load_mods.lua
+++ examples/webapp/load_mods.lua
@@ -26,7 +26,7 @@ return {
 		launch_num = 1,
 		default_arg = {
 			protocol = 'http',
-			port = 80,         --端口
+			port = 8688,         --端口
 			max_client = 30000, --最大连接数
 			second_conn_limit = 30000, --相同ip 1秒内建立连接数限制
 			keep_live_limit = 30000,  --相同ip 保持活跃数量限制
