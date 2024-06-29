fcd56bee7aca0c1a5e0e1894b75408cd15423625
huahua132 <958677003@qq.com>
2024-06-29 10:41:10 +0800

    fix 先于frpc启动无法推送消息的问题

--- lualib/skynet-fly/rpc/watch_server.lua
+++ lualib/skynet-fly/rpc/watch_server.lua
@@ -2,16 +2,28 @@ local skynet = require "skynet"
 
 local M = {}
 
+local g_frpc_server = nil
+
+local function get_frpc_addr()
+    if g_frpc_server then
+        return g_frpc_server
+    end
+    g_frpc_server = skynet.queryservice('frpc_server')
+    return g_frpc_server
+end
+
 --远程推送
 function M.publish(channel_name, ...)
     local msg, sz = skynet.pack(...)
-    skynet.send('.frpc_server', 'lua', "publish", channel_name, msg, sz)
+    local addr = get_frpc_addr()
+    skynet.send(addr, 'lua', "publish", channel_name, msg, sz)
 end
 
 --远程推送同步数据
 function M.pubsyn(channel_name, ...)
     local msg, sz = skynet.packstring(...)
-    skynet.send('.frpc_server', 'lua', "pubsyn", channel_name, msg, sz)
+    local addr = get_frpc_addr()
+    skynet.send(addr, 'lua', "pubsyn", channel_name, msg, sz)
 end
 
 return M
