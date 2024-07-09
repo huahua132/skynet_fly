d2422fc4ad968b68ffc57164b50c21b9e83f18af
huahua132 <958677003@qq.com>
2024-07-09 15:23:58 +0800

    codecache test 调整

--- examples/code_cache_test/main.lua
+++ examples/code_cache_test/main.lua
@@ -1,20 +1,30 @@
-local skynet = require "skynet"
-require "skynet.manager"
+local skynet = require "skynet.manager"
+local env_util = require "skynet-fly.utils.env_util"
+local log = require "skynet-fly.log"
+
 skynet.start(function()
-	-- local cur_time = skynet.time()
-	-- for i = 1,10000 do
-	-- 	skynet.newservice('hot_container_2','test_m')
-	-- end
-	-- skynet.error("use time:",skynet.time() - cur_time)
-	-- not cache 3.69 s
+	env_util.add_pre_load("./preload.lua")                --增加服务main.lua加载之前调用
 
-	local cur_time = skynet.time()
+	local cache_pre_time = skynet.time()
 	for i = 1,10000 do
-		skynet.newservice('test')
+		skynet.newservice('cachetest')
 	end
-	skynet.error("use time:",skynet.time() - cur_time)
+	local cache_over_time = skynet.time()
+	--cache 11 s
 
-	--cache 2.65 s
+	local nocache_pre_time = skynet.time()
+	for i = 1,10000 do
+		skynet.newservice('ncachetest')
+	end
+	local nocache_over_time = skynet.time()
 
-	--关闭code cache 启动服务性能下降28%
+	local now_time = skynet.time()
+
+	log.info("cachetest use time:",cache_over_time - cache_pre_time)
+	log.info("ncachetest use time:",nocache_over_time - nocache_pre_time)
+
+	--no cache 33 s
+
+	--关闭code cache 启动服务性能下降200% 下降比例会随着加载代码量增加而增加
+	--但是skynet_fly 并不推崇启动成千上万的服务数量
 end)
--- examples/code_cache_test/test.lua
+++
@@ -1,5 +0,0 @@
-local skynet = require "skynet"
-
-skynet.start(function()
-	
-end)
