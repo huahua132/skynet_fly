b1fcc6458921a81fb6061e8bdc3b9f18cd4202a6
huahua132 <958677003@qq.com>
2024-08-05 21:15:55 +0800

    suport spnet

---
+++ lualib/skynet-fly/utils/net/spnet_util.lua
@@ -0,0 +1,23 @@
+local socket = require "skynet.socket"
+local sp_netpack = require "skynet-fly.netpack.sp_netpack"
+local util_net_base = require "skynet-fly.utils.net.util_net_base"
+
+--------------------------------------------------------
+--这是给skynet gate网关服务处理消息用的 基于protobuf协议
+--------------------------------------------------------
+
+local M = {}
+
+--给fd发送socket消息
+M.send = util_net_base.create_gate_send(sp_netpack.pack)
+
+--给fd_list发送socket消息
+M.broadcast = util_net_base.create_gate_broadcast(sp_netpack.pack)
+
+--解包
+M.unpack = util_net_base.create_gate_unpack(sp_netpack.unpack)
+
+--客户端读取消息包
+M.recv = util_net_base.create_recv(socket.read,sp_netpack.unpack)
+
+return M 
--- test/module/proto_test_m.lua
+++ test/module/proto_test_m.lua
@@ -2,11 +2,13 @@ local log = require "skynet-fly.log"
 local timer = require "skynet-fly.timer"
 local skynet = require "skynet"
 local pb_netpack = require "skynet-fly.netpack.pb_netpack"
+local sproto = require "sproto"
+local core = require "sproto.core"
 
 local CMD = {}
 
-function CMD.start()
-	log.error("proto_test_m start!!!")
+local function protobuff_test()
+	log.error("protobuff_test start!!!")
 
 	log.info(pb_netpack.load("./proto"))
 
@@ -18,11 +20,45 @@ function CMD.start()
 	}
 
 	local ok,pb_str = pb_netpack.encode(".login.LoginReq",login_req)
-	log.info("pb.encode:",ok,pb_str)
+	log.info("pb.encode:",ok,#pb_str,pb_str)
 
 	log.info("pb.decode:",pb_netpack.decode(".login.LoginReq",pb_str))
 
-	log.error("proto_test_m start!!!")
+	log.error("protobuff_test end!!!")
+end
+
+local function sproto_test()
+	log.error("sproto_test start!!!")
+
+	local sproto_str = io.open('./sproto/login.sproto'):read('a')
+	local sp = sproto.parse(sproto_str)
+	core.dumpproto(sp.__cobj)
+	
+	for _, f in ipairs {"LoginReq", "LoginRes"} do
+		local def = sp:default(f)
+		print("default table for " .. f)
+		log.info(def)
+		print("--------------")
+	end
+
+	local login_req = {
+		player_id = 100001,
+		nickname = "skynet_fly",
+		password = "123456",
+		account = "skynet",
+	}
+	local pb_str = sp:encode("LoginReq", login_req)
+	log.info("sp encode:",#pb_str, pb_str)
+	log.info("sp pencode:", #sp:pencode("LoginReq", login_req))
+
+	log.info("sp decode:", sp:decode("LoginReq", pb_str))
+
+	log.error("sproto_test end!!!")
+end
+
+function CMD.start()
+	protobuff_test()
+	sproto_test()
 	return true
 end
 
