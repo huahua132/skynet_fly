6d761cc437e7b6855a3759535e65e42f08dcc79d
huahua132 <958677003@qq.com>
2024-09-26 16:26:02 +0800

    静态链接openssl zlib处理

--- install_centos.sh
+++ install_centos.sh
@@ -2,12 +2,13 @@
 
 # 安装编译skynet依赖的一些库
 install_dependencies() {
-	yum install -y git gcc autoconf automake make libtool curl centos-release-scl devtoolset-9-gcc* perl* cpan
+	yum install -y git gcc autoconf automake make libtool curl centos-release-scl devtoolset-9-gcc*
 	# centos8以上 dnf -y group install "Development Tools"
 }
 
 # 安装Perl
 install_perl() {
+	chmod -R 744 "3rd/perl"
 	cd "3rd/perl" || exit
 	(
 		source /opt/rh/devtoolset-9/enable
@@ -31,7 +32,7 @@ install_perl() {
 	}
 
 	echo "Perl $perl_version has been installed and linked successfully."
-	cd ..
+	cd ../../
 }
 
 # 编译openssl-3.3.2
@@ -39,16 +40,17 @@ install_openssl() {
 	# 获取脚本当前目录
 	CURRENT_DIR="$(dirname "$BASH_SOURCE")"
 	ABSOLUTE_PATH="$(realpath "$CURRENT_DIR/3rd/openssl")"
+	chmod -R 744 "3rd/openssl"
 	cd "3rd/openssl" || exit
 	(
-
+		rm -f *.a
 		echo "ABSOLUTE_PATH:$ABSOLUTE_PATH"
 		# 激活 devtoolset-9
 		source /opt/rh/devtoolset-9/enable && echo "Enabled devtoolset-9 successfully" || {
 			echo "Failed to enable devtoolset-9"
 			exit 1
 		}
-
+		
 		# 配置 OpenSSL
 		./config --prefix="$ABSOLUTE_PATH" -fPIC no-shared && echo "OpenSSL configured successfully" || {
 			echo "OpenSSL configuration failed"
@@ -60,34 +62,31 @@ install_openssl() {
 			echo "Make failed"
 			exit 1
 		}
-
-		# 安装
-		make install && echo "OpenSSL installed successfully" || {
-			echo "Make install failed"
-			exit 1
-		}
-
 		echo "OpenSSL installed successfully!"
 
 	)
-	cd ..
+	cd ../../
 }
 
 # 编译zlib-1.3.1
 install_zlib() {
+	chmod -R 744 "3rd/zlib"
 	cd "3rd/zlib" || exit
 	(
+		rm -f *.o libz.*
 		source /opt/rh/devtoolset-9/enable
-		gcc -c -fPIC -O2 ./*.c
-		ar rcs libz.a
+		./configure
+		make
 	)
-	cd ..
+	cd ../../
 }
 
 # 编译
 compile() {
 	(
 		source /opt/rh/devtoolset-9/enable
+		make cleanall
+		chmod -R 744 skynet
 		make linux
 	)
 }
