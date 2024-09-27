4e55af1fda4172ac3964ff3f50541d62d9294ea7
huahua132 <958677003@qq.com>
2024-09-27 15:40:37 +0800

    install 优化

--- install_centos.sh
+++ install_centos.sh
@@ -9,15 +9,14 @@ install_dependencies() {
 # 安装Perl
 install_perl() {
 	chmod -R 744 "3rd/perl"
-	cd "3rd/perl" || exit
-	(
-		source /opt/rh/devtoolset-9/enable
+	cd "3rd/perl"
+	source /opt/rh/devtoolset-9/enable
 
-		./Configure -des -Dprefix=$HOME/localperl
-		make
-		make test
-		make install
-	)
+	./Configure -des -Dprefix=$HOME/localperl
+	make -j4
+	make test -j4
+	make install
+
 	sudo mv /usr/bin/perl /usr/bin/perl.old && echo "Moved perl successfully" || {
 		echo "Failed to move perl"
 		exit 1
@@ -41,54 +40,47 @@ install_openssl() {
 	CURRENT_DIR="$(dirname "$BASH_SOURCE")"
 	ABSOLUTE_PATH="$(realpath "$CURRENT_DIR/3rd/openssl")"
 	chmod -R 744 "3rd/openssl"
-	cd "3rd/openssl" || exit
-	(
-		rm -f *.a
-		echo "ABSOLUTE_PATH:$ABSOLUTE_PATH"
-		# 激活 devtoolset-9
-		source /opt/rh/devtoolset-9/enable && echo "Enabled devtoolset-9 successfully" || {
-			echo "Failed to enable devtoolset-9"
-			exit 1
-		}
-		
-		# 配置 OpenSSL
-		./config --prefix="$ABSOLUTE_PATH" -fPIC no-shared && echo "OpenSSL configured successfully" || {
-			echo "OpenSSL configuration failed"
-			exit 1
-		}
+	cd "3rd/openssl"
+	rm -f *.a
+	echo "ABSOLUTE_PATH:$ABSOLUTE_PATH"
+	# 激活 devtoolset-9
+	source /opt/rh/devtoolset-9/enable && echo "Enabled devtoolset-9 successfully" || {
+		echo "Failed to enable devtoolset-9"
+		exit 1
+	}
+	
+	# 配置 OpenSSL
+	./config --prefix="$ABSOLUTE_PATH" -fPIC no-shared && echo "OpenSSL configured successfully" || {
+		echo "OpenSSL configuration failed"
+		exit 1
+	}
 
-		# 编译
-		make && echo "Make completed successfully" || {
-			echo "Make failed"
-			exit 1
-		}
-		echo "OpenSSL installed successfully!"
-
-	)
+	# 编译
+	make -j4 && echo "Make completed successfully" || {
+		echo "Make failed"
+		exit 1
+	}
+	echo "OpenSSL installed successfully!"
 	cd ../../
 }
 
 # 编译zlib-1.3.1
 install_zlib() {
 	chmod -R 744 "3rd/zlib"
-	cd "3rd/zlib" || exit
-	(
-		rm -f *.o libz.*
-		source /opt/rh/devtoolset-9/enable
-		./configure
-		make
-	)
+	cd "3rd/zlib"
+	rm -f *.o libz.*
+	source /opt/rh/devtoolset-9/enable
+	./configure
+	make -j4
 	cd ../../
 }
 
 # 编译
 compile() {
-	(
-		source /opt/rh/devtoolset-9/enable
-		make cleanall
-		chmod -R 744 skynet
-		make linux
-	)
+	source /opt/rh/devtoolset-9/enable
+	make cleanall
+	chmod -R 744 skynet
+	make linux -j4
 }
 
 # 主程序
--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -3,19 +3,17 @@
 # 安装编译skynet依赖的一些库
 install_dependencies() {
     apt-get update
-	apt-get install -y git gcc zlib1g-dev libssl-dev autoconf automake make libtool curl
+	apt-get install -y git gcc autoconf automake make libtool curl
 }
 
 # 安装Perl
 install_perl() {
 	chmod -R 744 "3rd/perl"
-	cd "3rd/perl" || exit
-	(
-		./Configure -des -Dprefix=$HOME/localperl
-		make
-		make test
-		make install
-	)
+	cd "3rd/perl"
+	./Configure -des -Dprefix=$HOME/localperl
+	make -j4
+	make test -j4
+	make install
 	sudo mv /usr/bin/perl /usr/bin/perl.old && echo "Moved perl successfully" || {
 		echo "Failed to move perl"
 		exit 1
@@ -39,47 +37,40 @@ install_openssl() {
 	CURRENT_DIR="$(dirname "$BASH_SOURCE")"
 	ABSOLUTE_PATH="$(realpath "$CURRENT_DIR/3rd/openssl")"
 	chmod -R 744 "3rd/openssl"
-	cd "3rd/openssl" || exit
-	(
-		rm -f *.a
-		echo "ABSOLUTE_PATH:$ABSOLUTE_PATH"
-		
-		# 配置 OpenSSL
-		./config --prefix="$ABSOLUTE_PATH" -fPIC no-shared && echo "OpenSSL configured successfully" || {
-			echo "OpenSSL configuration failed"
-			exit 1
-		}
+	cd "3rd/openssl"
+	rm -f *.a
+	echo "ABSOLUTE_PATH:$ABSOLUTE_PATH"
+	
+	# 配置 OpenSSL
+	./config --prefix="$ABSOLUTE_PATH" -fPIC no-shared && echo "OpenSSL configured successfully" || {
+		echo "OpenSSL configuration failed"
+		exit 1
+	}
 
-		# 编译
-		make && echo "Make completed successfully" || {
-			echo "Make failed"
-			exit 1
-		}
-		echo "OpenSSL installed successfully!"
-
-	)
+	# 编译
+	make -j4 && echo "Make completed successfully" || {
+		echo "Make failed"
+		exit 1
+	}
+	echo "OpenSSL installed successfully!"
 	cd ../../
 }
 
 # 编译zlib-1.3.1
 install_zlib() {
 	chmod -R 744 "3rd/zlib"
-	cd "3rd/zlib" || exit
-	(
-		rm -f *.o libz.*
-		./configure
-		make
-	)
+	cd "3rd/zlib"
+	rm -f *.o libz.*
+	./configure
+	make -j4
 	cd ../../
 }
 
 # 编译
 compile() {
-	(
-		make cleanall
-		chmod -R 744 skynet
-		make linux
-	)
+	make cleanall
+	chmod -R 744 skynet
+	make linux -j4
 }
 
 # 主程序
