9926d3372cce73be3fba83cf9c6a88902b414e01
huahua132 <958677003@qq.com>
2024-09-28 15:34:11 +0800

    删除perl多余文件

--- 3rd/perl/cpan/Test-Simple/lib/Test2/AsyncSubtest/Formatter.pm
+++
@@ -1,9 +0,0 @@
-package Test2::AsyncSubtest::Formatter;
-use strict;
-use warnings;
-
-our $VERSION = '1.302204';
-
-die "Should not load this anymore";
-
-1;
--- 3rd/perl/cpan/Test-Simple/lib/Test2/Util/Table/Cell.pm
+++
@@ -1,9 +0,0 @@
-package Test2::Util::Table::Cell;
-use strict;
-use warnings;
-
-our $VERSION = '1.302204';
-
-use base 'Term::Table::Cell';
-
-1;
