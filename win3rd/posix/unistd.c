90dabcb3e984c28750d7cdfaa9decc72095d21a5
huahua132 <958677003@qq.com>
2024-11-19 10:31:46 +0800

    window下时间问题

--- win3rd/posix/unistd.c
+++ win3rd/posix/unistd.c
@@ -40,8 +40,23 @@ void sleep(size_t ms) { Sleep(ms); }
 int clock_gettime(int what, struct timespec* ti) {
     switch (what) {
     case CLOCK_MONOTONIC:
+        static __int64 Freq = 0;
+        static __int64 Start = 0;
+        static __int64 StartTime = 0;
+        if (Freq == 0) {
+            StartTime = time(NULL);
+            QueryPerformanceFrequency((LARGE_INTEGER *)&Freq);
+            QueryPerformanceCounter((LARGE_INTEGER *)&Start);
+        }
+        __int64 Count = 0;
+        QueryPerformanceCounter((LARGE_INTEGER *)&Count);
+
+        //乘以1000，把秒化为毫秒
+        __int64 now =(__int64)((double)(Count - Start) / (double)Freq * 1000.0) + StartTime * 1000;
+        ti->tv_sec = now / 1000;
+        ti->tv_nsec = (now - now / 1000 * 1000) * 1000 * 1000;
+        return 0;
     case CLOCK_REALTIME:
-    case CLOCK_THREAD_CPUTIME_ID: {
         SYSTEMTIME st;
         GetSystemTime(&st); // 获取 UTC 时间
 
@@ -55,6 +70,20 @@ int clock_gettime(int what, struct timespec* ti) {
         ti->tv_sec = (uint32_t)((u64.QuadPart - 116444736000000000ULL) / 10000000); // 转换为秒
         ti->tv_nsec = (uint32_t)((u64.QuadPart % 10000000) * 100); // 获取纳秒部分
         return 0; // 响应成功
+    case CLOCK_THREAD_CPUTIME_ID: {
+        // 获取当前线程的 CPU 时间
+        FILETIME creation_time, exit_time, kernel_time, user_time;
+        if (GetThreadTimes(GetCurrentThread(), &creation_time, &exit_time, &kernel_time, &user_time)) {
+            ULARGE_INTEGER u64;
+            u64.LowPart = user_time.dwLowDateTime;
+            u64.HighPart = user_time.dwHighDateTime;
+
+            ti->tv_sec = (uint32_t)((u64.QuadPart - 116444736000000000ULL) / 10000000); // 转换为秒
+            ti->tv_nsec = (uint32_t)((u64.QuadPart % 10000000) * 100); // 获取纳秒部分
+            return 0;
+        } else {
+            return -1; // 获取失败
+        }
     default:
         return 3;
     } break;
