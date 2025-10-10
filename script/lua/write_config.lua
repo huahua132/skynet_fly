8ac246816466f2c28468eeddbb5762a51bda8ea2
huahua132 <958677003@qq.com>
2025-10-10 09:38:04 +0800

    优化write_config可填充任意skynet_env参数

--- script/lua/write_config.lua
+++ script/lua/write_config.lua
@@ -1,4 +1,5 @@
 local assert = assert
+local pairs = pairs
 local ARGV = { ... }
 local skynet_fly_path = ARGV[1]
 local load_mods_name = ARGV[2]
@@ -64,11 +65,13 @@ local config = {
 
 	lua_path        = "",
 	enablessl       = true,
-	loadmodsfile    = load_mods_name, --可热更服务启动配置
-	recordfile      = recordfile,		 --播放录像的文件名
-	recordlimit     = 1024 * 1024 * 100, --录像记录限制(字节数) 超过不再写录像
-	machine_id      = 1,				 --机器ID(全局唯一)
-	trace			= 0,				 --链路追踪
+	loadmodsfile    = load_mods_name,     --可热更服务启动配置
+	recordfile      = recordfile,		  --播放录像的文件名
+	recordlimit     = 1024 * 1024 * 100,  --录像记录限制(字节数) 超过不再写录像
+	machine_id      = 1,				  --机器ID(全局唯一)
+	trace			= 0,				  --链路追踪
+	certfile        = "./server-cert.pem",--ssl 证书相关
+	keyfile 		= "./server-key.pem",
 }
 
 if package.config:sub(1, 1) == '\\' then
@@ -88,56 +91,8 @@ end
 
 if load_mods_f and load_mods_f.share_config_m and load_mods_f.share_config_m.default_arg and load_mods_f.share_config_m.default_arg.server_cfg then
 	local cfg = load_mods_f.share_config_m.default_arg.server_cfg
-	if cfg.svr_name then
-		config.svr_name = cfg.svr_name
-	end
-	if cfg.svr_type then
-		config.svr_type = cfg.svr_type
-	end
-	if cfg.svr_id then
-		config.svr_id = cfg.svr_id
-	end
-	if cfg.thread then
-		config.thread = cfg.thread
-	end
-	if cfg.logpath then
-		config.logpath = cfg.logpath
-	end
-	if cfg.loglevel then
-		config.loglevel = cfg.loglevel
-	end
-	if cfg.logfilename then
-		config.logfilename = cfg.logfilename
-	end
-	if cfg.debug_port then
-		config.debug_port = cfg.debug_port
-	end
-	if cfg.breakpoint_debug_host then										--断点调式连接host
-		config.breakpoint_debug_host = cfg.breakpoint_debug_host
-	end
-	if cfg.breakpoint_debug_port then										--断点调式连接port
-		config.breakpoint_debug_port = cfg.breakpoint_debug_port
-	end
-	if cfg.breakpoint_debug_module_name then								--断点调式的可热更模块名
-		config.breakpoint_debug_module_name = cfg.breakpoint_debug_module_name
-	end	
-	if cfg.breakpoint_debug_module_index then								--断点调式的可热更模块启动下标
-		config.breakpoint_debug_module_index = cfg.breakpoint_debug_module_index
-	end
-	if cfg.log_is_launch_rename ~= nil then
-		config.log_is_launch_rename = cfg.log_is_launch_rename				--启动是否重命名旧日志
-	end
-	if cfg.recordpath then
-		config.recordpath = cfg.recordpath									--录像文件目录
-	end
-	if cfg.recordlimit then													--录像记录限制
-		config.recordlimit = cfg.recordlimit
-	end
-	if cfg.machine_id then
-		config.machine_id = cfg.machine_id
-	end
-	if cfg.trace then
-		config.trace = cfg.trace
+	for k, v in pairs(cfg) do
+		config[k] = v
 	end
 end
 
